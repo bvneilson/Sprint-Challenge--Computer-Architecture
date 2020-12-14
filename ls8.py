ff84d390b2839e460296e0bf29d4cc693a4fa5cf
bvneilson <bvneilson@gmail.com>
2020-12-14 11:58:54 -0700

    ls8 passing tests

---
+++ ls8.py
@@ -0,0 +1,15 @@
+import sys
+from cpu import *
+
+if len(sys.argv) != 2:
+    print("Usage: ls8.py 'valid file path'")
+
+else:
+    try:
+        cpu = CPU()
+
+        cpu.load(sys.argv[1])
+        cpu.run()
+
+    except FileNotFoundError:
+        print(f"Couldn't open {sys.argv[1]}")
